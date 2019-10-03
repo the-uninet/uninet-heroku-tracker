7f07d4fc188132bce2592f7f5c7ec516a2e647e1
zaoqi <zaomir@outlook.com>
2019-10-03 17:09:52 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const WebSocket = require('ws')
 
-require('http').createServer((req, res)=>{
-  res.write('Hi')
-  res.end()
-}).listen(process.env.PORT)
+const wss = new WebSocket.Server({ port: process.env.PORT })
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('received: %s', message)
+  })
+
+  ws.send('something');
+})
